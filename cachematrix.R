52daab6ba3ea26c137a6e0cf0f8cd0ed4ce9e1b0
Vunabandi <66242594+Vunabandi@users.noreply.github.com>
2020-09-03 03:29:53 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,30 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix catches the inverse of a matrix with the object linv
+makeCacheMatrix <- function(x = matrix(sample(1:124,25),5,5)) {
+  linv <- NULL
+  set <- function(y) {
+    x <<- y
+    linv <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) linv <<- solve
+  getsolve <- function() linv
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
 ## Write a short comment describing this function
-
+## when makeCacheMatrix does not work, we can rely on cacheSolve which does the same thing
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  linverse <- x$getsolve()
+  if(!is.null(linverse)) {
+    message("getting inversed matrix")
+    return(linverse)
+  }
+  data <- x$get()
+  linverse <- solve(data, ...)
+  x$setsolve(linverse)
+  linverse
 }
